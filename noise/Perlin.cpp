d792f8afc2a6de7ac6187e39e2b01528b5825391
Dirk Zimmermann <me@dirkz.com>
2025-09-16 09:09:53 +0200

    Start Perlin::Noise

--- noise/Perlin.cpp
+++ noise/Perlin.cpp
@@ -1,1 +1,12 @@
 #include "Perlin.h"
+
+constexpr int GridLength = 256;
+
+float Perlin::Noise(float x, float y, float z)
+{
+    int x0 = static_cast<int>(x) % GridLength;
+    int y0 = static_cast<int>(y) % GridLength;
+    int z0 = static_cast<int>(z) % GridLength;
+
+    return 0.f;
+}
--- noise/Perlin.h
+++ noise/Perlin.h
@@ -2,4 +2,17 @@
 
 struct Perlin
 {
+
+    template <class T> T fade(T x)
+    {
+        // 6x^5 - 15x^4 + 10^3
+        return x * x * x * (x * (6 * x - 15) + 10);
+    }
+
+    template <class T> T lerp(T a, T b, T t)
+    {
+        return a + t * b;
+    }
+
+    float Noise(float x, float y, float z);
 };
