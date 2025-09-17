9edad372409656ec94a333899394f0eb007057ca
Dirk Zimmermann <me@dirkz.com>
2025-09-17 13:55:49 +0200

    Fade for XMVECTOR

--- noise/Perlin.h
+++ noise/Perlin.h
@@ -4,18 +4,30 @@ struct Perlin
 {
     Perlin();
 
-    template <class T> T Fade(T x)
-    {
-        // 6x^5 - 15x^4 + 10^3
-        return x * x * x * (x * (6 * x - 15) + 10);
-    }
-
     template <class T> T Lerp(T t, T a, T b)
     {
         return a + t * b;
     }
 
     float Noise(float x, float y, float z);
+
+    /// 6x^5 - 15x^4 + 10^3
+    inline DirectX::XMVECTOR Fade(DirectX::FXMVECTOR v)
+    {
+        using namespace DirectX;
+
+        static const XMVECTOR V15 = XMVectorReplicate(15.f);
+        static const XMVECTOR V10 = XMVectorReplicate(10.f);
+
+        XMVECTOR vv = XMVectorScale(v, 6.f);
+        vv = XMVectorSubtract(vv, V15);
+        vv = XMVectorMultiply(v, vv);
+        vv = XMVectorAdd(vv, V10);
+        XMVECTOR v3 = XMVectorMultiply(v, XMVectorMultiply(v, v));
+        vv = XMVectorMultiply(v3, vv);
+
+        return vv;
+    }
 
   private:
     int Hash(int x, int y, int z);
