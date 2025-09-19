5b4acdf8f459ab4326de37593a29df2552650942
Dirk Zimmermann <me@dirkz.com>
2025-09-19 10:46:51 +0200

    Keep the naming original, except for the Pascal naming

--- noise/Perlin.cpp
+++ noise/Perlin.cpp
@@ -4,7 +4,7 @@ using namespace DirectX;
 
 constexpr int GridLength = 256;
 
-constexpr std::array<int, 512> Permutations{
+constexpr std::array<int, 512> P{
     151, 160, 137, 91,  90,  15,  131, 13,  201, 95,  96,  53,  194, 233, 7,   225, 140, 36,  103,
     30,  69,  142, 8,   99,  37,  240, 21,  10,  23,  190, 6,   148, 247, 120, 234, 75,  0,   26,
     197, 62,  94,  252, 219, 203, 117, 35,  11,  32,  57,  177, 33,  88,  237, 149, 56,  87,  174,
@@ -155,7 +155,7 @@ float Perlin::Noise(float x, float y, float z)
 
 int Perlin::Hash(int x, int y, int z)
 {
-    auto i = (x + Permutations[(y + Permutations[z]) % GridLength]) % GridLength;
+    auto i = (x + P[(y + P[z]) % GridLength]) % GridLength;
     return i;
 }
 
