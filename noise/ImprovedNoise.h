6c7aaf75e1005f1fb4de4223ff5dc91cfd068318
Dirk Zimmermann <me@dirkz.com>
2025-09-19 10:44:17 +0200

    Permutations, fade and lerp

--- noise/ImprovedNoise.h
+++ noise/ImprovedNoise.h
@@ -4,4 +4,13 @@
 
 struct ImprovedNoise
 {
+    template <class T> static T fade(T t)
+    {
+        return t * t * t * (t * (t * T{6} - T{15}) + T{10});
+    }
+
+    template <class T> static T lerp(T t, T a, T b)
+    {
+        return a + t * (b - a);
+    }
 };
