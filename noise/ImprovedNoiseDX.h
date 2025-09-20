d06c268e1fb166b992a4b808ed5f44a1ac826560
Dirk Zimmermann <me@dirkz.com>
2025-09-20 11:55:44 +0200

    ImprovedNoiseDX with vector gradients

---
+++ noise/ImprovedNoiseDX.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "stdafx.h"
+
+struct ImprovedNoiseDX
+{
+    template <class T> static T Fade(T t)
+    {
+        return t * t * t * (t * (t * T{6} - T{15}) + T{10});
+    }
+
+    template <class T> static T Lerp(T t, T a, T b)
+    {
+        return a + t * (b - a);
+    }
+
+    static double Grad(int hash, double x, double y, double z);
+
+    static double Noise(double x, double y, double z);
+};
