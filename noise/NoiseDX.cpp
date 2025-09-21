737333bfec389623acc7ada98f00126cf8ce9c79
Dirk Zimmermann <me@dirkz.com>
2025-09-21 13:31:01 +0200

    Reuse the one and only P

--- noise/NoiseDX.cpp
+++ noise/NoiseDX.cpp
@@ -9,37 +9,6 @@ using namespace DirectX;
 
 constexpr bool OriginalGradients = true;
 
-constexpr std::array<int, 512> P{
-    151, 160, 137, 91,  90,  15,  131, 13,  201, 95,  96,  53,  194, 233, 7,   225, 140, 36,  103,
-    30,  69,  142, 8,   99,  37,  240, 21,  10,  23,  190, 6,   148, 247, 120, 234, 75,  0,   26,
-    197, 62,  94,  252, 219, 203, 117, 35,  11,  32,  57,  177, 33,  88,  237, 149, 56,  87,  174,
-    20,  125, 136, 171, 168, 68,  175, 74,  165, 71,  134, 139, 48,  27,  166, 77,  146, 158, 231,
-    83,  111, 229, 122, 60,  211, 133, 230, 220, 105, 92,  41,  55,  46,  245, 40,  244, 102, 143,
-    54,  65,  25,  63,  161, 1,   216, 80,  73,  209, 76,  132, 187, 208, 89,  18,  169, 200, 196,
-    135, 130, 116, 188, 159, 86,  164, 100, 109, 198, 173, 186, 3,   64,  52,  217, 226, 250, 124,
-    123, 5,   202, 38,  147, 118, 126, 255, 82,  85,  212, 207, 206, 59,  227, 47,  16,  58,  17,
-    182, 189, 28,  42,  223, 183, 170, 213, 119, 248, 152, 2,   44,  154, 163, 70,  221, 153, 101,
-    155, 167, 43,  172, 9,   129, 22,  39,  253, 19,  98,  108, 110, 79,  113, 224, 232, 178, 185,
-    112, 104, 218, 246, 97,  228, 251, 34,  242, 193, 238, 210, 144, 12,  191, 179, 162, 241, 81,
-    51,  145, 235, 249, 14,  239, 107, 49,  192, 214, 31,  181, 199, 106, 157, 184, 84,  204, 176,
-    115, 121, 50,  45,  127, 4,   150, 254, 138, 236, 205, 93,  222, 114, 67,  29,  24,  72,  243,
-    141, 128, 195, 78,  66,  215, 61,  156, 180, // first 256
-    151, 160, 137, 91,  90,  15,  131, 13,  201, 95,  96,  53,  194, 233, 7,   225, 140, 36,  103,
-    30,  69,  142, 8,   99,  37,  240, 21,  10,  23,  190, 6,   148, 247, 120, 234, 75,  0,   26,
-    197, 62,  94,  252, 219, 203, 117, 35,  11,  32,  57,  177, 33,  88,  237, 149, 56,  87,  174,
-    20,  125, 136, 171, 168, 68,  175, 74,  165, 71,  134, 139, 48,  27,  166, 77,  146, 158, 231,
-    83,  111, 229, 122, 60,  211, 133, 230, 220, 105, 92,  41,  55,  46,  245, 40,  244, 102, 143,
-    54,  65,  25,  63,  161, 1,   216, 80,  73,  209, 76,  132, 187, 208, 89,  18,  169, 200, 196,
-    135, 130, 116, 188, 159, 86,  164, 100, 109, 198, 173, 186, 3,   64,  52,  217, 226, 250, 124,
-    123, 5,   202, 38,  147, 118, 126, 255, 82,  85,  212, 207, 206, 59,  227, 47,  16,  58,  17,
-    182, 189, 28,  42,  223, 183, 170, 213, 119, 248, 152, 2,   44,  154, 163, 70,  221, 153, 101,
-    155, 167, 43,  172, 9,   129, 22,  39,  253, 19,  98,  108, 110, 79,  113, 224, 232, 178, 185,
-    112, 104, 218, 246, 97,  228, 251, 34,  242, 193, 238, 210, 144, 12,  191, 179, 162, 241, 81,
-    51,  145, 235, 249, 14,  239, 107, 49,  192, 214, 31,  181, 199, 106, 157, 184, 84,  204, 176,
-    115, 121, 50,  45,  127, 4,   150, 254, 138, 236, 205, 93,  222, 114, 67,  29,  24,  72,  243,
-    141, 128, 195, 78,  66,  215, 61,  156, 180, // mirror of first 256
-};
-
 constexpr float PointW = 1.f;
 constexpr float VectorW = 0.f;
 constexpr float GradientPlus = +1.f;
@@ -149,8 +118,10 @@ float Noise(float x, float y, float z)
     float v = fFaded.y;
     float w = fFaded.z;
 
-    int A = P[X] + Y, AA = P[A] + Z, AB = P[A + 1] + Z,     // HASH COORDINATES OF
-        B = P[X + 1] + Y, BA = P[B] + Z, BB = P[B + 1] + Z; // THE 8 CUBE CORNERS,
+    int A = ImprovedNoise::P[X] + Y, AA = ImprovedNoise::P[A] + Z,
+        AB = ImprovedNoise::P[A + 1] + Z, // HASH COORDINATES OF
+        B = ImprovedNoise::P[X + 1] + Y, BA = ImprovedNoise::P[B] + Z,
+        BB = ImprovedNoise::P[B + 1] + Z; // THE 8 CUBE CORNERS,
 
     const XMVECTOR MinusX = XMVectorSet(-1, 0, 0, 0);
     const XMVECTOR MinusY = XMVectorSet(0, -1, 0, 0);
@@ -162,17 +133,19 @@ float Noise(float x, float y, float z)
 
     return ImprovedNoise::Lerp(
         w,
-        ImprovedNoise::Lerp(v,
-                            ImprovedNoise::Lerp(u, Grad0(P[AA], p),                      // AND ADD
-                                                Grad0(P[BA], XMVectorAdd(p, MinusX))),   // BLENDED
-                            ImprovedNoise::Lerp(u, Grad0(P[AB], XMVectorAdd(p, MinusY)), // RESULTS
-                                                Grad0(P[BB], XMVectorAdd(p, MinusXY)))), // FROM  8
         ImprovedNoise::Lerp(
             v,
-            ImprovedNoise::Lerp(u, Grad0(P[AA + 1], XMVectorAdd(p, MinusZ)), // CORNERS
-                                Grad0(P[BA + 1], XMVectorAdd(p, MinusXZ))),  // OF CUBE
-            ImprovedNoise::Lerp(u, Grad0(P[AB + 1], XMVectorAdd(p, MinusYZ)),
-                                Grad0(P[BB + 1], XMVectorAdd(p, MinusXYZ)))));
+            ImprovedNoise::Lerp(u, Grad0(ImprovedNoise::P[AA], p),                      // AND ADD
+                                Grad0(ImprovedNoise::P[BA], XMVectorAdd(p, MinusX))),   // BLENDED
+            ImprovedNoise::Lerp(u, Grad0(ImprovedNoise::P[AB], XMVectorAdd(p, MinusY)), // RESULTS
+                                Grad0(ImprovedNoise::P[BB], XMVectorAdd(p, MinusXY)))), // FROM  8
+        ImprovedNoise::Lerp(
+            v,
+            ImprovedNoise::Lerp(
+                u, Grad0(ImprovedNoise::P[AA + 1], XMVectorAdd(p, MinusZ)), // CORNERS
+                Grad0(ImprovedNoise::P[BA + 1], XMVectorAdd(p, MinusXZ))),  // OF CUBE
+            ImprovedNoise::Lerp(u, Grad0(ImprovedNoise::P[AB + 1], XMVectorAdd(p, MinusYZ)),
+                                Grad0(ImprovedNoise::P[BB + 1], XMVectorAdd(p, MinusXYZ)))));
 }
 
 } // namespace NoiseDX
