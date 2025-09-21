348a128c1e369c7f159896400c0a8310129dda46
Dirk Zimmermann <me@dirkz.com>
2025-09-21 13:00:42 +0200

    Templated Grad

--- noise/ImprovedNoise.h
+++ noise/ImprovedNoise.h
@@ -47,10 +47,10 @@ struct ImprovedNoise
         return a + t * (b - a);
     }
 
-    static double Grad(int hash, double x, double y, double z)
+    template <class T> static T Grad(int hash, T x, T y, T z)
     {
-        int h = hash & 15;        // CONVERT LO 4 BITS OF HASH CODE
-        double u = h < 8 ? x : y, // INTO 12 GRADIENT DIRECTIONS.
+        int h = hash & 15;   // CONVERT LO 4 BITS OF HASH CODE
+        T u = h < 8 ? x : y, // INTO 12 GRADIENT DIRECTIONS.
             v = h < 4                ? y
                 : h == 12 || h == 14 ? x
                                      : z;
